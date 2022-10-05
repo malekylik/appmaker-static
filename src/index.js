e18fe41f964ad5aa2e845d578ba38a948cae9c4e
Maksim Kalinouski <kalinouski@google.com>
2022-10-05 11:43:16 +0000

    Base impl

---
+++ src/index.js
@@ -0,0 +1,26 @@
+exports.RiskDriverSubCategory = {
+  "Financial": [
+    "Revenue Loss/Expense",
+    "Financial Reporting"
+  ],
+  "Operational": [
+    "Volumetric Drivers",
+    "Regional Impact",
+    "System Disruption",
+    "Data Management"
+  ],  
+  "Reputational": [
+    "Press sentiment",
+    "Customer Satisfaction"
+  ],     
+  "Legal / Complicance": [
+    "Legal / Regulatory Compliance",
+    "Regulatory Scrutinyn with list of categories to be confirmed by PGA"
+  ],
+  "No selection": []
+};
+
+exports.onSelectRiskDriverCategory = function() {
+  app.viewFragments.AddRDriverDialog.descendants.RiskDriverSubCategoryFieldInput2.value = "No selection";
+};
+
