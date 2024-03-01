4ddad28765e8bb4c64841a2d60fcea525bbcbadd
Maksim Kalinouski <kalinouski@google.com>
2024-03-01 12:49:27 +0000

    Add prefix for generated script files to match AppMaker output

--- dist/generate.js
+++ dist/generate.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateResultXML = void 0;
 function generateResultXML(initXML, res) {
-    return (`<script key="${initXML.script.key}" type="${initXML.script.type}" name="${initXML.script.name}"><![CDATA[${res}]]></script>`);
+    const prefix = '\n'; // Appmaker puts a new line in the begining of scipts
+    return prefix +
+        (`<script key="${initXML.script.key}" type="${initXML.script.type}" name="${initXML.script.name}"><![CDATA[${res}]]></script>`);
 }
 exports.generateResultXML = generateResultXML;
--- src/generate.ts
+++ src/generate.ts
@@ -1,5 +1,7 @@
 import { ScriptFile } from './appmaker';
 
 export function generateResultXML(initXML: ScriptFile, res: string) {
-  return (`<script key="${initXML.script.key}" type="${initXML.script.type}" name="${initXML.script.name}"><![CDATA[${res}]]></script>`);
+  const prefix = '\n'; // Appmaker puts a new line in the begining of scipts
+  return prefix +
+    (`<script key="${initXML.script.key}" type="${initXML.script.type}" name="${initXML.script.name}"><![CDATA[${res}]]></script>`);
 }
